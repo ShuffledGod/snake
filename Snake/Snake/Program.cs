a19315b1a4e7a8234778c36abb10d21caccfe724
ShuffledGod <mihail.podkhvatilin@mail.ru>
2015-12-21 23:38:32 +0300

    2. lesson

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x1 = 1;
+            int y1=5;
+            char sym1 = '*';
+
+            Drow(x1, y1, sym1);
+                        
             Console.ReadLine();
+        }
+
+        static void Drow(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
         }
     }
 }
